3314065facb2e497a9c5e4d50a2c498854e34102
MehedizYT <mdmarajahmed81@gmail.com>
2026-07-04 13:30:38 +0600

    Change default REQUIRED_CHANNEL value
    Updated REQUIRED_CHANNEL to default to '@gexomeme'.

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
 # Optional Settings
 # Leave empty if not required
-REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "") # e.g. "@MyChannel"
+REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "@gexomeme") # e.g. "@MyChannel"
 X_ACCOUNT = os.getenv("X_ACCOUNT", "") # e.g. "https://x.com/username"
 
 # Star System Pricing (e.g., 50 Stars for Premium)
